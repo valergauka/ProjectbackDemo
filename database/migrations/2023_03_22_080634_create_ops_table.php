63c0f2b55d0ae1131546406510e588dcd41dc2fe
Lera Gauka <vgauka00@gmail.com>
2023-03-24 16:36:55 +0200

    Initial commit

---
+++ database/migrations/2023_03_22_080634_create_ops_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ops', function (Blueprint $table) {
+            $table->id();
+            $table->string('branch');
+            $table->string('speciality');
+            $table->string('specialisation');
+            $table->string('op');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ops');
+    }
+};
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Review;
+use App\Models\Comment;
+use App\Models\Op;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::get('/review', function (){
+    $review = Review::all();
+    return $review;
+});
+
+Route::get('/comment', function (){
+    $comment = Comment::all();
+    return $comment;
+});
+
+Route::get('/op', function (){
+    $op = Op::all();
+    return $op;
+});
